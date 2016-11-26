75ff7ed227a7e4cd87401fab4060efe13a04de74
Soner Ercelik <soner.ercelik@tum.de>
2016-11-26 12:05:27 -0800

    Spalte mit zugehoerigem Zeitintervall hinzugefuegt

--- traffic_analysis_master.R
+++ traffic_analysis_master.R
@@ -33,6 +33,16 @@ data$Local <- ifelse(data$Driver.State == "MD", TRUE, FALSE)
 data = select(data, -Driver.State) # Remove Driver.State
 
 # Add a column to see at which weekday the violation occured
-data = mutate(data, weekDay = paste( wday(as.Date(trafficData$Date.Of.Stop, format = "%m/%d/%Y"), label = TRUE)  ))
+data = mutate(data, weekDay = paste( wday(as.Date(data$Date.Of.Stop, format = "%m/%d/%Y"), label = TRUE)  ))
+
+# Add column time intervall: 
+# 1. Create column with converted time in seconds
+convertedInSecTimeIntervall = minutes(times(data$Time.Of.Stop)) + hours(times(data$Time.Of.Stop)) * 60 * 60
+# 2. Assign the seconds in intervall
+cuts <- c(-Inf, 10800, 10800*2, 10800*3, 10800*4, 10800*5, 10800*6, 10800*7, Inf)
+labs <- c("'00:00 - 02:59'", "03:00 - 05:59", "06:00 - 08:59", "09:00 - 11:59", "12:00 - 14:59", "15:00 - 17:59", "18:00 - 20:59", "21:00 - 23:59")
+assignedInterval = labs[findInterval(convertedInSecTimeIntervall, cuts)]
+# 3. Add column with useful values into data
+data = mutate(data, timeIntervall = paste (assignedInterval))
 
 #---------- END DATA PREPARATION ----------#
