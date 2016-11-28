bf92502d2f606ae31de2690549c01ff3f3d78c10
Christoph Erl <erl.christoph@gmail.com>
2016-11-27 20:12:08 -0800

    Master file updated

--- traffic_analysis_master.R
+++ traffic_analysis_master.R
@@ -2,6 +2,25 @@
 library(sp)
 library(raster)
 library(dplyr)
+library(ggmap)
+
+library(devtools)
+install_version("ggplot2", version = "2.1.0", repos = "http://cran.us.r-project.org")
+
+#---------- START HELPER METHODS ----------#
+
+preparePolygon <- function(x) {
+  rtp <- rasterToPolygons(x)
+  rtp@data$id <- 1:nrow(rtp@data) # add id column for join
+  
+  rtpFort <- fortify(rtp, data = rtp@data)
+  rtpFortMer <- merge(rtpFort, rtp@data, by.x = 'id', by.y = 'id')  # join data
+  
+  return(rtpFortMer)
+}
+
+#---------- END HELPER METHODS ----------#
+#---------- START READING DATA ----------#
 
 # File Names
 dataCSVFileName = "traffic_violations.csv"
@@ -16,6 +35,7 @@ if(!file.exists(dataRDSFileName)) {
 # Read Data
 data.raw = readRDS(dataRDSFileName)
 
+#---------- END READING DATA ----------#
 #---------- START DATA PREPARATION ----------#
 
 # Remove redundant location information
@@ -36,7 +56,6 @@ data$Local <- ifelse(data$Driver.State == "MD", TRUE, FALSE)
 data = select(data, -Driver.State) # Remove Driver.State
 
 #---------- END DATA PREPARATION ----------#
-
 #---------- START DATA VISUALIZATION ----------#
 
 # Traffic Violation Hotspots Analysis using raster package
@@ -47,30 +66,35 @@ data = select(data, -Driver.State) # Remove Driver.State
 coords = select(data, Latitude, Longitude)
 coords = mutate(coords, count = 1)
 
-r.xmn = quantile(coords[,2], probs=0.05)
-r.xmx = quantile(coords[,2], probs=0.95)
-r.ymn = quantile(coords[,1], probs=0.05)
-r.ymx = quantile(coords[,1], probs=0.95)
-
-r = raster(ncol=100, nrow=100, xmn=-77.4, xmx=-77.0, ymn=38.9, ymx=39.4) # xmn=r.xmn, xmx=r.xmx, ymn=r.ymn, ymx=r.ymx)
+r = raster(ncol=25, nrow=25, xmn=-77.5, xmx=-76.9, ymn=38.95, ymx=39.35)
 r.rasterized = rasterize(coords[,2:1], r, coords[,3], fun = sum)
+r.treshold = 0
+values(r.rasterized) = ifelse(values(r.rasterized) < r.treshold, NA, values(r.rasterized))
 
-us = getData("GADM", country="USA", level=2)
-maryland = subset(us, NAME_1 == "Maryland")
+r.values = values(r.rasterized); r.values = ifelse(r.values < r.treshold, NA, r.values)
+r.topvalues = tail(sort(r.values), 10)
+r.topvalues.indexes = match(r.topvalues, r.values)
+r.topvalues.coordinates = xyFromCell(r.rasterized, cell = r.topvalues.indexes)
+#r.topvalues.adresses = apply(r.topvalues.coordinates, 1, function(v) revgeocode(v))
 
-r.values = values(r.rasterized); r.values = r.values[!is.na(r.values)]
-r.quantiles = c(.2,.4,.6,.8)
-breakpoints <- c(quantile(r.values, probs=0.2), quantile(r.values, probs=0.5), quantile(r.values, probs=1))
-colors <- c("transparent", "red")
+map.maryland    = subset(getData("GADM", country="USA", level=2), NAME_1 == "Maryland")
+
+polygon.montgomery  = ggmap(get_map(location = "Montgomery County", maptype = "roadmap", zoom = 10), extent = "device")
+polygon.maryland    = geom_polygon(data=maryland, aes(x=long, y=lat), alpha = 0.1, fill = NA, color = "black") 
+polygon.raster      = geom_polygon(data=preparePolygon(r.rasterized), aes(x = long, y = lat, group = group, fill = layer), size = 0, alpha=0.3) 
+polygon.grid        = geom_line(data=preparePolygon(r), aes(x = long, y = lat, group = group), color = "gray47", size = 0.2) 
+polygon.legend      = scale_fill_gradientn(limits = c(r.treshold, max(r.values, na.rm = T)), colours = topo.colors(255))
+
+map.default = polygon.montgomery + polygon.maryland 
+map.grid = polygon.montgomery + polygon.maryland + polygon.grid
+map.violations = polygon.montgomery + polygon.maryland + polygon.grid + polygon.raster + polygon.legend
+
+ggsave(map.default, file = "figures/map_default.png", width = 5, height = 4, type = "cairo-png")
+ggsave(map.grid, file = "figures/map_grid.png", width = 5, height = 4, type = "cairo-png")
+ggsave(map.violations, file = "figures/map_violations.png", width = 5, height = 4, type = "cairo-png")
 
 png(filename="maryland_traffic.png", height=1000, width=1000, bg="NA")
-
-values(r.rasterized)
-
-xyFromCell(r.rasterized, cell = 2)
-
-plot(r.rasterized) # breaks = breakpoints, col = colors)
-plot(maryland, add = TRUE)
+apply(r.topvalues.coordinates, 1, function(v) points(v[1], v[2], col = "black", cex = .6))
 dev.off()
 
 # Add a column to see at which weekday the violation occured
@@ -91,3 +115,11 @@ freq_timeInteravl <- data.frame(table(data$timeInterval))
 plot(freq_timeInteravl, type="l", xlab="Time Interval", ylab="Frequency")
 
 #---------- END DATA VISUALIZATION ----------#
+
+
+
+
+
+
+
+
